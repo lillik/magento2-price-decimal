a632c630b02ac954331d69ebff514286fe9700eb
Calin Diacon <calin.diacon@dotmailer.com>
2018-03-22 12:07:37 +0000

    force the price to not use 2 digitgs round

--- Model/Plugin/PriceCurrency.php
+++ Model/Plugin/PriceCurrency.php
@@ -14,21 +14,82 @@ class PriceCurrency extends PriceFormatPluginAbstract
     /**
      * {@inheritdoc}
      */
-    public function aroundFormat(
+    public function beforeFormat(
         \Magento\Directory\Model\PriceCurrency $subject,
-        callable $proceed,
         ...$args
     ) {
 
         if ($this->getConfig()->isEnable()) {
-
-            if(!isset($args[1])){
+            // add the optional arg
+            if(! isset($args[1])){
                 $args[1] = true;
             }
-
-            $args[2] = $this->getPricePrecision(); // Precision argument
+            // Precision argument
+            $args[2] = $this->getPricePrecision();
         }
 
-        return $proceed(...$args);
+        return $args;
+    }
+
+    /**
+     * @param \Magento\Directory\Model\PriceCurrency $subject
+     * @param callable $proceed
+     * @param $price
+     * @param array ...$args
+     * @return float
+     */
+    public function aroundRound(
+        \Magento\Directory\Model\PriceCurrency $subject,
+        callable $proceed,
+        $price,
+        ...$args
+    )
+    {
+        if ($this->getConfig()->isEnable()) {
+            return round($price, $this->getPricePrecision());
+
+        } else {
+            return $proceed($price);
+        }
+    }
+
+    /**
+     * @param \Magento\Directory\Model\PriceCurrency $subject
+     * @param array ...$args
+     * @return array
+     */
+    public function beforeConvertAndFormat(
+        \Magento\Directory\Model\PriceCurrency $subject,
+        ...$args
+    )
+    {
+        if ($this->getConfig()->isEnable()) {
+            // add the optional args
+            $args[1] = isset($args[1])? $args[1] : null;
+            $args[2] = isset($args[2])? $args[2] : null;
+            $args[3] = $this->getPricePrecision();
+        }
+
+        return $args;
+    }
+
+    /**
+     * @param \Magento\Directory\Model\PriceCurrency $subject
+     * @param array ...$args
+     * @return array
+     */
+    public function beforeConvertAndRound(
+        \Magento\Directory\Model\PriceCurrency $subject,
+        ...$args
+    )
+    {
+        if ($this->getConfig()->isEnable()) {
+            //add optional args
+            $args[1] = isset($args[1])? $args[1] : null;
+            $args[2] = isset($args[2])? $args[2] : null;
+            $args[3] = $this->getPricePrecision();
+        }
+
+        return $args;
     }
 }
