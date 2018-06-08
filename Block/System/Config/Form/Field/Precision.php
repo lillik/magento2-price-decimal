60d9089ea86acb5f7ae4eb8a24d88e4708339e64
Lilian <lilian.codreanu@gmail.com>
2018-06-08 16:24:29 +0300

    Add Precision option

---
+++ Block/System/Config/Form/Field/Precision.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * @category   Evozon
+ * @package    Magento_Evozon
+ * @copyright  Copyright (c) 2018 Evozon (http://www.evozon.com)
+ * @author     Lilian Codreanu <lilian.codreanu@evozon.com>
+ */
+
+namespace Lillik\PriceDecimal\Block\System\Config\Form\Field;
+
+
+class Precision implements \Magento\Framework\Option\ArrayInterface
+{
+
+    /**
+     * Return array of options as value-label pairs
+     *
+     * @return array Format: array(array('value' => '<value>', 'label' => '<label>'), ...)
+     */
+    public function toOptionArray()
+    {
+        return [
+            ['value' => 1, 'label' => __('1')],
+            ['value' => 2, 'label' => __('2')],
+            ['value' => 3, 'label' => __('3')],
+            ['value' => 4, 'label' => __('4')],
+        ];
+    }
+}
