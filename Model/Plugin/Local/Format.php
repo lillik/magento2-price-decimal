4e353e5b01259a5c8c247f79d2811f1a07cc3faf
Quinten <quinten@justbetter.nl>
2021-03-05 16:09:51 +0100

    Fix currency precision not going to javascript

--- Model/Plugin/Local/Format.php
+++ Model/Plugin/Local/Format.php
@@ -23,7 +23,7 @@ class Format extends PriceFormatPluginAbstract
      */
     public function afterGetPriceFormat($subject, $result)
     {
-        $precision = $this->getPricePrecision();
+        $precision = $this->getPricePrecisionCurrency();
 
         if ($this->getConfig()->isEnable()) {
             $result['precision'] = $precision;
