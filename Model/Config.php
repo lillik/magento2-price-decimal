538473255edc5834f4389cb3f86169ee3c56546b
Patrick Allaire, ptre <foi02@cartefoi.net>
2021-03-04 21:22:16 -0500

    Includes Roy Nilsson (Aug 2021)  and Patriboom modifications (mar 2021)

--- Model/Config.php
+++ Model/Config.php
@@ -2,46 +2,46 @@
 /**
  *
  * @package Lillik\PriceDecimal\Model
- *
  * @author  Lilian Codreanu <lilian.codreanu@gmail.com>
+ * includes Roy Nilsson's modifications ( Aug 2020 ) 
+ * includes Patriboom's modifications   ( Mar 2021 ) 
  */
 
 namespace Lillik\PriceDecimal\Model;
 
 use Magento\Framework\App\Config\ScopeConfigInterface;
+use Magento\Framework\App\RequestInterface;
 
 class Config implements ConfigInterface
 {
 
-    const XML_PATH_PRICE_PRECISION
-        = 'catalog_price_decimal/general/price_precision';
+	const XML_PATH_GENERAL_ENABLE 			= 'catalog_price_decimal/general/enable';
+	const XML_PATH_CAN_SHOW_PRICE_DECIMAL 	= 'catalog_price_decimal/general/can_show_decimal';
+	const XML_PATH_DISABLE_FOR_ACTIONS 		= 'catalog_price_decimal/general/disable_for_actions';
 
-    const XML_PATH_PRICE_PRECISIONCART
-        = 'catalog_price_decimal/general/price_precisionCart';
+	const XML_PATH_PRICE_PRECISION 			= 'catalog_price_decimal/general/price_precision';
+	const XML_PATH_PRICE_PRECISIONCART 		= 'catalog_price_decimal/general/price_precisionCart';
+	const XML_PATH_PRICE_PRECISIONCHECKOUT = 'catalog_price_decimal/general/price_precisionCheckout';
 
-    const XML_PATH_PRICE_PRECISIONCHECKOUT
-        = 'catalog_price_decimal/general/price_precisionCheckout';
-
-    const XML_PATH_CAN_SHOW_PRICE_DECIMAL
-        = 'catalog_price_decimal/general/can_show_decimal';
-
-    const XML_PATH_GENERAL_ENABLE
-        = 'catalog_price_decimal/general/enable';
 
     /**
      * @var \Magento\Framework\App\Config\ScopeConfigInterface
      */
     private $scopeConfig;
 
+	/**
+     * @var \Magento\Framework\App\RequestInterface
+     */
+    private $request;
+    
     /**
      * @param ScopeConfigInterface $scopeConfig
      */
-    public function __construct(
-        ScopeConfigInterface $scopeConfig
-    ) {
-
+    public function __construct( ScopeConfigInterface $scopeConfig, RequestInterface $request ) 
+		{
         $this->scopeConfig = $scopeConfig;
-    }
+        $this->request = $request;
+    	}
 
     /**
      * @return \Magento\Framework\App\Config\ScopeConfigInterface
@@ -68,7 +68,8 @@ class Config implements ConfigInterface
      */
     public function isEnable()
     {
-        return $this->getValueByPath(self::XML_PATH_GENERAL_ENABLE, 'website');
+      //return $this->getValueByPath(self::XML_PATH_GENERAL_ENABLE, 'website');
+		return $this->getValueByPath(self::XML_PATH_GENERAL_ENABLE, 'website') && !$this->isDisabledForAction(); 
     }
 
     /**
@@ -95,5 +96,16 @@ class Config implements ConfigInterface
     public function getPricePrecisionCheckout()
     {
         return $this->getValueByPath(self::XML_PATH_PRICE_PRECISIONCHECKOUT, 'website');
+    }
+
+    private function isDisabledForAction()
+    {
+        $currentAction = $this->request->getModuleName() . '_' . $this->request->getControllerName() . '_' . $this->request->getActionName();
+        foreach (explode(',', $this->getValueByPath(self::XML_PATH_DISABLE_FOR_ACTIONS, 'website')) as $action) {
+            if (trim($action) == $currentAction) {
+                return true;
+            }
+        }
+        return false;
     }
 }
